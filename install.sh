406f7caa7f3d074b7495f115e9b11e06d2e35b15
Sanny Sanoff <san@sysdate.com>
2023-02-19 19:03:51 +0100

    added ninja for android

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ export MAKEOPTS=-j$(nproc)
 
 # Install dependencies
 apt update -y
-apt install -y wget git p7zip-full default-jdk build-essential cmake patchelf python3-mako
+apt install -y wget git p7zip-full default-jdk build-essential cmake patchelf python3-mako ninja-build
 
 # Install the Android tools
 mkdir Android
